37621029785f04ab1ebd4f020b68336826422d09
Steve Chakif <schakif@Steves-MacBook-Pro.local>
2014-01-31 15:21:22 -0500

    first commit

--- Quiz3/main.m
+++ Quiz3/main.m
@@ -13,6 +13,70 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz3AppDelegate class]));
+        //return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz3AppDelegate class]));
+        NSLog(@"Steven");
+        NSString *firstName = @"Steven";
+        NSLog(@"%@", firstName);
+        NSNumber *val = @200;
+        NSLog(@"%@", val);
+        
+        NSArray *fruits = @[@"Apple", @"Grape", @"Pear"];
+        for (NSString *fruit in fruits){
+            NSLog(@"%@", fruit);
+        }
+        
+        NSDictionary *fruitSweetness = @{@"Apple": @3, @"Grape":@5, @"Pear":@8};
+        NSLog(@"%@", fruitSweetness[@"Grape"]);
+        NSUInteger myUInteger = 123;
+        NSLog(@"%lu", (unsigned long)myUInteger);
+        
+        NSNumber *a = @10;
+        NSNumber *b = @4;
+        NSUInteger aInt = [a unsignedIntegerValue];
+        NSUInteger bInt = [b unsignedIntegerValue];
+        NSUInteger ab = aInt * bInt;
+        NSLog(@"%lu", (unsigned long)ab);
+        
+        NSString *lastName = @"Chakif";
+        NSString *fullName = [[firstName stringByAppendingString:@" "] stringByAppendingString:lastName];
+        NSLog(@"%@", fullName);
+        
+        for (NSString *fruit in fruitSweetness){
+            NSNumber *rating = fruitSweetness[fruit];
+            if ([rating intValue] < 4){
+                NSLog(@"%@ Is Not Sweet", fruit);
+            } else if ([rating intValue] < 7) {
+                NSLog(@"%@ is SomewhatSweet", fruit);
+            } else {
+                NSLog(@"%@ is Sweet", fruit);
+            }
+        }
+        
+        typedef NS_ENUM(NSInteger, DayOfWeek){
+            MON = 1,
+            TUE = 2,
+            WED = 3,
+            THU = 4,
+            FRI = 5,
+            SAT = 6,
+            SUN = 7
+            
+        };
+        
+        DayOfWeek currentDay;
+        
+        void(^myFirstBlock)(void) = ^{
+            NSLog(@"Hello World");
+        };
+        myFirstBlock();
+        
+        void(^mySecondBlock)(NSString *) = ^(NSString *str) {
+            NSLog(@"%@", str);
+        };
+        
+        mySecondBlock(@"Hello");
+        mySecondBlock(@"World");
+        
+        
     }
 }
